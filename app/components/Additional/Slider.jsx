6325c26487cf841bcdaf048d39ae25ecaa7f5c84
mohan-16 <mohanrajg1630@gmail.com>
2025-08-12 20:40:30 +0530

    Brand Details updated

--- app/components/Additional/Slider.jsx
+++ app/components/Additional/Slider.jsx
@@ -1,28 +1,44 @@
 "use client";
 import Image from "next/image";
+import jio from "@/public/assets/brands/jio.png";
+import qq from "@/public/assets/brands/q&q.png";
+import avalon from "@/public/assets/brands/avalon.png";
+import saavari from "@/public/assets/brands/saavari.png";
+import techmahindra from "@/public/assets/brands/techmahindra.png";
 
 export default function TechStackMarquee() {
   const logos = [
-    { src: "/opencv.png", alt: "OpenCV" },
-    { src: "/scikit.png", alt: "Scikit-learn" },
-    { src: "/angular.png", alt: "Angular" },
-    { src: "/vercel.png", alt: "Vercel" },
-    { src: "/firebase.png", alt: "Firebase" },
-    { src: "/opencv.png", alt: "OpenCV" },
-    { src: "/scikit.png", alt: "Scikit-learn" },
-    { src: "/angular.png", alt: "Angular" },
+    { src: jio, alt: "Jio" },
+    { src: qq, alt: "Q&Q" },
+    { src: avalon, alt: "Avalon" },
+    { src: saavari, alt: "Saavari" },
+    { src: techmahindra, alt: "Tech Mahindra" },
+    { src: jio, alt: "Jio" },
+    { src: qq, alt: "Q&Q" },
+    { src: avalon, alt: "Avalon" },
+    { src: saavari, alt: "Saavari" },
+    { src: techmahindra, alt: "Tech Mahindra" },
   ];
 
   return (
     <div className="bg-white pt-10">
       {/* <h2 className="text-center text-xl font-bold mb-4">
        — Our Technology Stack —
-      </h2> */}
+  </h2> */}
       <div className="overflow-hidden">
         <div className="flex whitespace-nowrap animate-scroll">
           {[...logos, ...logos].map((logo, index) => (
-            <div key={index} className="flex-shrink-0 logo-item">
-              <Image src={logo.src} alt={logo.alt} width={80} height={80} />
+            <div
+              key={index}
+              className="flex-shrink-0 logo-item flex items-center justify-center h-20"
+            >
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                width={80}
+                height={80}
+                className="w-20"
+              />
             </div>
           ))}
         </div>

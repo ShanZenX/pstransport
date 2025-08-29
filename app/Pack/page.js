9868fe0b4d60444dd934554559a2b4a6f9b26bca
shanmugarajsekar <shanmugaraj2003@gmail.com>
2025-08-29 21:35:12 +0530

    pack page created

---
+++ app/Pack/page.js
@@ -0,0 +1,5 @@
+export default function PackPage() {
+    return <div className="px-4 pt-6 bg-[#e6b31900]">
+        <h1 className="text-3xl font-bold mb-4 text-center">Pack Page</h1>
+    </div> ;
+}
